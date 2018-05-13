80daa8a8b1e94a081196d2a8304e70032b3669dd
Nero <neroshan12@gmail.com>
2018-05-13 13:11:48 +0100

    Add test file for getting all jobs

--- test/routes.jobs.test.js
+++ test/routes.jobs.test.js
@@ -7,12 +7,23 @@ const server = require('../server')
 describe('GET api/v1/jobs', () => {
     it('should return 200 OK', (done) => {
         chai.request(server)
-        .get(`/api/v1/jobs`)
-        .end((err, res) => {
-            should.not.exist(err)
-            console.log(err)
-            res.status.should.equal(200)
-            done();
-        })
+            .get(`/api/v1/jobs`)
+            .end((err, res) => {
+                should.not.exist(err)
+                res.status.should.equal(200)
+                done();
+            })
     });
+    it('should return all jobs', () => {
+        chai.request(server)
+            .get(`/api/v1/jobs`)
+            .end((err, res) => {
+                should.not.exist(err)
+                res.body.data[0].should.include.keys(
+                    'business', 'role', 'closing_date', 'location', 'link'
+                )
+                res.status.should.equal(200)
+                done();
+            })
+    })
 });
