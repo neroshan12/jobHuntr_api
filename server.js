af2cea5f8a25b2131a51d69e982f530554c98dc1
Nero <neroshan12@gmail.com>
2018-05-13 12:24:47 +0100

    Rename server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const path = require("path")
+const jobRoutes = require('./src/routes/jobs')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000
+
+app.use(`/api/v1`, jobRoutes)
+
+app.listen(PORT, () => {
+    console.log(`APP RUNNING ON PORT ${PORT}`)}
+);
+
+module.exports = app;
